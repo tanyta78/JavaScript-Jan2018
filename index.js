c762ddc60a53c236656824cd8581390e4be890df
Tanya Milanova <tanyta78@gmail.com>
2018-01-31 22:13:09 +0200

    map set lab

--- index.js
+++ index.js
@@ -111,10 +111,210 @@ function countWords(arr) {
 }
 
 //6.	Count Words with Maps
-function countWordsWithMap(params) {
-    
+function countWordsWithMap(arr) {
+    let text = arr.join('\n');
+    let words = text.split(/[^A-Za-z0-9_]/)
+        .filter(w => w != '')
+        .map(w=>w.toLowerCase());
+    let wordsCount = new Map();
+    words.forEach(w => {
+        wordsCount.has(w) ? wordsCount.set(w, wordsCount.get(w) + 1) : wordsCount.set(w, 1);
+    });
+
+    let sorted = Array.from(wordsCount.keys()).sort();
+    for (let key of sorted) {
+        console.log("'" + key + "'" + " -> " + wordsCount.get(key) + " times");
+    }
 }
+
+countWordsWithMap(['Far too slow, you re far too slow.']);
+
+//7.	Populations in Towns
+/*You have been tasked to create a register for different towns and their population.
+The input comes as array of strings. Each element will contain data for a town and its population in the following format:
+“{townName} <-> {townPopulation}”
+If you receive the same town twice, you should add the given population to the current one.
+
+*/
+function populationInTownsWithObj(arrOfStrings) {
+    let towns = {};
+    for (let line of arrOfStrings) {
+        let [town, pop] = line.split(' <-> ');
+        if (!towns.hasOwnProperty(town)) {
+            towns[town] = 0;
+        }
+        towns[town] += Number(pop);
+    }
+
+
+    for (let town of Object.keys(towns)) {
+        console.log(`${town} : ${towns[town]}`);
+
+    }
+
+
+
+
+}
+
+function populationInTownsWithMap(arrOfStrings) {
+    let towns = new Map();
+    for (let line of arrOfStrings) {
+        let [town, pop] = line.split(' <-> ');
+        if (!towns.has(town)) {
+            towns.set(town, 0);
+        }
+        towns.set(town, towns.get(town) + Number(pop));
+    }
+
+    [...towns].forEach(([town, pop]) => console.log(`${town} : ${pop}`));
+
+}
+
+//8.	City Markets
+/*You have been tasked to follow the sales of products in the different towns. For every town you need to keep track of all the products sold, and for every product, the amount of total income.
+The input comes as array of strings. Each element will represent data about a product and its sales. The format of input is:
+{town} -> {product} -> {amountOfSales} : {priceForOneUnit}
+The town and product are both strings. The amount of sales and price for one unit will be numbers. Store all towns, for every town, store its products, and for every product, its amount of total income. The total income is calculated with the following formula - amount of sales * price for one unit. If you receive as input a town you already have, you should just add the new product to it.
+As output you must print every town, its products and their total income in the following format:
+“Town – {townName}
+*/
+function cityMarkets(strArr) {
+    let cityMarkets = new Map();
+    for (let line of strArr) {
+        let [town, product, sales] = line.split(' -> ');
+        sales = sales.split(' : ').reduce((a, b) => a * b);
+
+        if (!cityMarkets.has(town)) {
+            cityMarkets.set(town, new Map());
+        }
+
+        if (!cityMarkets.get(town).has(product)) {
+            cityMarkets.get(town).set(product, 0);
+        }
+
+        let oldSales = cityMarkets.get(town).get(product);
+        cityMarkets.get(town).set(product, oldSales + sales);
+    }
+
+    for (let [town, products] of cityMarkets) {
+        console.log(`Town - ${town}`);
+
+        for (let [product, sales] of products) {
+            console.log(`$$$${product} : ${sales}`);
+
+        }
+    }
+
+}
+
+//10.	Extract Unique Words
+/*Write a JS function that extracts all UNIQUE words from a valid text, and stores them. Ensure that there are NO duplicates in the stored words. Once you find a word, there is no need for you to store it again if you meet it again in the text. You also need to make all characters from the words you’ve stored – lowercase.
+*/
+function uniqueWordsInText(arrStr) {
+    let text = arrStr.join('\n');
+    let unique = new Set();
+    let words = text.split(/\W+/)
+        .filter(w => w !== '')
+        .map(e => e.toLowerCase())
+        .forEach(e => unique.add(e));
+
+    console.log([...unique].join(', '));
+
+}
+
+//9.	Lowest Prices in Cities
+/*You will be given several towns, with products and their price. You need to find the lowest price for every product and the town it is sold at for that price.
+The input comes as array of strings. Each element will hold data about a town, product, and its price at that town. The town and product will be strings; the price will be a number. The input will come in the following format:
+{townName} | {productName} | {productPrice}
+If you receive the same town and product more than once, you should update the old value with the new one.
+As output you must print each product with its lowest price and the town at which the product is sold at that price. If two towns share the same lowest price, print the one that was entered first. 
+The output, for every product, should be in the following format:
+{productName} -> {productLowestPrice} ({townName})
+The order of output is – order of entrance. See the examples for more info.
+
+*/
+function lowestPriceInCities(arr) {
+    let products = new Map();
+
+    for (let row of arr) {
+        let [town, product, price] = row.split(' | ');
+
+        //check if product exist
+        if (!products.has(product)) {
+            products.set(product, new Map());
+        }
+
+        //check if town exist
+        if (!products.get(product).has(town)) {
+            products.get(product).set(town, 0);
+        }
+
+        products.get(product).set(town, Number(price));
+    }
+
+    for (let [product, prices] of products) {
+
+        let minPrice = Math.min(...prices.values());
+        let townName = getKeyByValue(prices, minPrice);
+        console.log(`${product} -> ${minPrice} (${townName})`);
+
+
+    }
+
+    function getKeyByValue(mymap, value) {
+        return [...mymap.keys()].find(key => mymap.get(key) === value);
+    }
+    //     let sorted = [...products].forEach(([product,cities])=>[...cities.entries()].map(e =>{ return e[1];}).sort(function(a, b) {return a-b;}));
+
+    //       [...products].forEach(([product,cities])=>console.log(`${product} -> ${([...cities].map(e=>{return e[1];}))[0]} (${([...cities].map(e=>{return e[0];}))[0]})`));
+    // //console.log(products);
+
+}
+
+
+
 /*
+lowestPriceInCities([
+    'Sofia City | Audi | 100000',
+    'Sofia City | BMW | 100000',
+    'Sofia City | Mitsubishi | 10000',
+    'Sofia City | Mercedes | 10000',
+    'Sofia City | NoOffenseToCarLovers | 0',
+    'Mexico City | Audi | 1000',
+    'Mexico City | BMW | 99999',
+    'New York City | Mitsubishi | 10000',
+    'New York City | Mitsubishi | 1000',
+    'Mexico City | Audi | 100000',
+    'Washington City | Mercedes | 1000',
+]);
+uniqueWordsInText([
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis hendrerit dui.', 
+    'Quisque fringilla est urna, vitae efficitur urna vestibulum fringilla. ',
+    'Vestibulum dolor diam, dignissim quis varius non, fermentum non felis. ',
+    'Vestibulum ultrices ex massa, sit amet faucibus nunc aliquam ut. ',
+    'Morbi in ipsum varius, pharetra diam vel, mattis arcu. ',
+    'Integer ac turpis commodo, varius nulla sed, elementum lectus. ',
+    'Vivamus turpis dui, malesuada ac turpis dapibus, congue egestas metus.',    
+]);
+cityMarkets([
+    'Sofia -> Laptops HP -> 200 : 2000',
+    'Sofia -> Raspberry -> 200000 : 1500',
+    'Sofia -> Audi Q7 -> 200 : 100000',
+    'Montana -> Portokals -> 200000 : 1',
+    'Montana -> Qgodas -> 20000 : 0.2',
+    'Montana -> Chereshas -> 1000 : 0.3'
+]);
+
+populationInTownsWithObj([
+    'Sofia <-> 1200000',
+    'Montana <-> 20000',
+    'New York <-> 10000000',
+    'Washington <-> 2345000',
+    'Las Vegas <-> 1000000'
+
+]);
+
 console.log(sumByTown(['Sofia','20', 'Varna','10', 'Sofia','5']));
 
 console.log(jsonToHtml('[{"Name":"Tomatoes & Chips","Price":2.35},{"Name":"J&B Chocolate","Price":0.96}]'));
